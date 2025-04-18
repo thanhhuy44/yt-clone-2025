0cc1bb31460fca1520ccac0a2c52c462fc2fdfa9
thanhhuy44 <thhuy28062001@gmail.com>
2025-04-18 15:12:29 +0700

    Database setup

---
+++ drizzle.config.ts
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
+
+dotenv.config({
+    path: ".env.local"
+})
+
+export default defineConfig({
+  out: './drizzle',
+  schema: './src/db/schema.ts',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
---
+++ src/db/schema.ts
@@ -0,0 +1,17 @@
+import {  pgTable, uuid, text, timestamp , uniqueIndex} from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  clerkId: text("clerk_id").notNull().unique(),
+  name: text("name").notNull(),
+  //   TODO: add banner field
+  imageUrl: text("image_url").notNull(),
+    createdAt: timestamp("created_at", {
+    withTimezone: true,
+    mode: "string",
+  }).defaultNow().notNull(),
+  updatedAt: timestamp("updated_at", {
+    withTimezone: true,
+    mode: "string",
+  }).defaultNow().notNull()
+}, (t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)]);
